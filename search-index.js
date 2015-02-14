1035b8ee00dcb693809a62231907aa624c6ef6cd
Hugo Duncan <hugo@hugoduncan.org>
2015-02-14 16:47:59 -0500

    Initial doc commit

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['from_error_scope'] = {"items":[[0,"","from_error_scope","Enable conversion between non-local `Error` types."],[8,"FromErrorScope","","A FromError like trait, that can be implemented on an explicit\nScope type, enabling conversion between non-local Error types."],[10,"from_error","","",0],[14,"trys!","","A try like macro that takes an additional type argument for a type\nthat implements FromErrorScope.  You may wish to wrap this macro\nfor a specific error scope, to shorten invocations."]],"paths":[[8,"FromErrorScope"]]};
+initSearch(searchIndex);
